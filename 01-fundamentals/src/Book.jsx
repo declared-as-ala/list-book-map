601e8ba27de3e6d124fb1442073f41366f459b39
ahmedbenhamouda1 <you-can-put-ahmed-email@example.com>
2026-02-16 11:59:50 +0100

    Update to JSX files and push to new repository

--- 01-fundamentals/src/Book.jsx
+++ 01-fundamentals/src/Book.jsx
@@ -1,12 +1,12 @@
 const Book = (props) => {
-  const { img, title, author } = props;
+  // const { img, title, author } = props;
 
   return (
     <article className='book'>
-      <img src={img} alt={title} />
+      {/* <img src={img} alt={title} />
       <h2>{title}</h2>
 
-      <h4>{author} </h4>
+      <h4>{author} </h4> */}
     </article>
   );
 };
